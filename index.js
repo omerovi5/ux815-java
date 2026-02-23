d5ff9764b8e28b5db617dac35f12588f61ae5c04
omerovi5 <123518803+omerovi5@users.noreply.github.com>
2026-02-23 03:20:13 +0000

    scroll test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-ScrollReveal().reveal('.img');
+ScrollReveal().reveal('.reveal-img', {
+    distance: '50px',
+    duration: 800,
+    easing: 'ease-in-out',
+    origin: 'bottom',
+    interval: 200 
+  });
